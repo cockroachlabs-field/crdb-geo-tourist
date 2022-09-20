53e2eefd1c52e5ea3aa8bc080cf74284aab8de66
mgoddard <mgoddard@cockroachlabs.com>
2022-09-20 09:56:43 -0400

    Removing stale CA cert files

--- k8s/psql_cli.sh
+++ k8s/psql_cli.sh
@@ -2,8 +2,13 @@
 
 lb_ip=$( kubectl describe service crdb-lb | perl -ne 'chomp; print "$1\n" if /^LoadBalancer Ingress:\s+((\d+\.){3}\d+)/;' )
 
-# Get the CA cert if necessary
-[ -f "/tmp/ca.crt" ] || ./get_ca_cert.sh
+cert="/tmp/ca.crt"
 
-psql "postgresql://tourist:tourist@${lb_ip}:26257/defaultdb?sslmode=require&sslrootcert=/tmp/ca.crt"
+# Remove any stale cert file
+find $cert -mmin +20 -exec rm -f {} \;
+
+# Get the CA cert if necessary
+[ -f $cert ] || ./get_ca_cert.sh
+
+psql "postgresql://tourist:tourist@${lb_ip}:26257/defaultdb?sslmode=require&sslrootcert=$cert"
 
