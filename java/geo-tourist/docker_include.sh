129b5461ca54cee98d44839cef058c04ff48fad0
mgoddard <mgoddard@cockroachlabs.com>
2023-04-21 13:18:55 -0400

    Fixing the 'ApplicationName' JDBC parameter and explicitly setting Hikari's connection lifetime

--- java/geo-tourist/docker_include.sh
+++ java/geo-tourist/docker_include.sh
@@ -1,3 +1,3 @@
-tag=1.3
+tag=1.4
 docker_id="mgoddard"
 img_name="boot-geo-tourist"
--- java/geo-tourist/env.sh
+++ java/geo-tourist/env.sh
@@ -1,4 +1,4 @@
-export DB_URL="jdbc:cockroachdb://localhost:26257/defaultdb?user=root&ssl=false&application_name=BootGeoTourist&retryConnectionErrors=true&retryTransientErrors=true"
+export DB_URL="jdbc:cockroachdb://localhost:26257/defaultdb?user=root&ssl=false&ApplicationName=Geo%20Tourist%20Boot&retryConnectionErrors=true&retryTransientErrors=true"
 
 # The server.port value will be set to this value:
 export FLASK_PORT=8081
