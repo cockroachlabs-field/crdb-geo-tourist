30c20134eb0e454a1fbf46cb58703afadcdccdca
mgoddard <mgoddard@cockroachlabs.com>
2023-04-21 16:33:47 -0400

    Enabling follower reads at the session level via JDBC connection string

--- java/geo-tourist/env.sh
+++ java/geo-tourist/env.sh
@@ -1,4 +1,4 @@
-export DB_URL="jdbc:cockroachdb://localhost:26257/defaultdb?user=root&ssl=false&ApplicationName=Geo%20Tourist%20Boot&retryConnectionErrors=true&retryTransientErrors=true"
+export DB_URL="jdbc:cockroachdb://localhost:26257/defaultdb?user=root&ssl=false&ApplicationName=Geo%20Tourist%20Boot&retryConnectionErrors=true&retryTransientErrors=true&options=-c%20default_transaction_use_follower_reads%3Don"
 
 # The server.port value will be set to this value:
 export FLASK_PORT=8081
